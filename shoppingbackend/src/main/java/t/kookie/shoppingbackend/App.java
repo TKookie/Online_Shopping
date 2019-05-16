01e5399555451c611960109b7ddb4259acf96abf
t shashini <tharushi925@gmail.com>
2019-05-16 16:02:54 +0530

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/t/kookie/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package t.kookie.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
